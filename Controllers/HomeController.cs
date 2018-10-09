883efb2e37da0893266981fe10c296fff3ad75e2
Phamthien0809 <43979515+Phamthien0809@users.noreply.github.com>
2018-10-09 11:20:48 +0700

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult SoTietKiemLui()
+        {
+            ViewBag.Message = "STK.";
+
+            return View();
+        }
+        public ActionResult LyDoLuaChon()
+        {
+            ViewBag.Message = "Ly Do Lua Chon.";
+
+            return View();
+        }
+        public ActionResult PhuongThucThanhToan()
+        {
+            ViewBag.Message = "PhuongThucThanhToan.";
+
+            return View();
+        }
+    }
+}
